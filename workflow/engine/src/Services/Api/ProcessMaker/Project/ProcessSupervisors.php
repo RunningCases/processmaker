36ab9b03991006abeb8b3291454d67c34dd99889
Freddy Daniel Rojas Valda <daniel.rojas@colosa.com>
2014-01-07 12:29:16 -0400

    Se adiciona available Process supervisor para dynaform e inputdocuments

--- workflow/engine/src/Services/Api/ProcessMaker/Project/ProcessSupervisors.php
+++ workflow/engine/src/Services/Api/ProcessMaker/Project/ProcessSupervisors.php
@@ -35,28 +35,6 @@ class ProcessSupervisors extends Api
 
     /**
      * @param string $prjUid {@min 32} {@max 32}
-     * @param string $filter
-     * @param int    $start
-     * @param int    $limit
-     *
-     * @url GET /:prjUid/available-supervisors
-     */
-    public function doGetAvailableSupervisors($prjUid, $filter = '', $start = null, $limit = null)
-    {
-        try {
-            $supervisor = new \BusinessModel\ProcessSupervisor();
-            $arrayData = $supervisor->getAvailableSupervisors($prjUid, $filter, $start, $limit);
-            //Response
-            $response = $arrayData;
-        } catch (\Exception $e) {
-            //response
-            throw new RestException(Api::STAT_APP_EXCEPTION, $e->getMessage());
-        }
-        return $response;
-    }
-
-    /**
-     * @param string $prjUid {@min 32} {@max 32}
      *
      * @url GET /:prjUid/inputdocument-supervisor
      */
@@ -92,6 +70,68 @@ class ProcessSupervisors extends Api
         }
         return $response;
     }
+
+    /**
+     * @param string $prjUid {@min 32} {@max 32}
+     * @param string $filter
+     * @param int    $start
+     * @param int    $limit
+     *
+     * @url GET /:prjUid/available-supervisors
+     */
+    public function doGetAvailableSupervisors($prjUid, $filter = '', $start = null, $limit = null)
+    {
+        try {
+            $supervisor = new \BusinessModel\ProcessSupervisor();
+            $arrayData = $supervisor->getAvailableSupervisors($prjUid, $filter, $start, $limit);
+            //Response
+            $response = $arrayData;
+        } catch (\Exception $e) {
+            //response
+            throw new RestException(Api::STAT_APP_EXCEPTION, $e->getMessage());
+        }
+        return $response;
+    }
+
+    /**
+     * @param string $prjUid {@min 32} {@max 32}
+     *
+     * @url GET /:prjUid/available-dynaform-supervisor
+     */
+    public function doGetAvailableDynaformSupervisor($prjUid)
+    {
+        try {
+            $supervisor = new \BusinessModel\ProcessSupervisor();
+            $arrayData = $supervisor->getAvailableDynaformSupervisors($prjUid);
+            //Response
+            $response = $arrayData;
+        } catch (\Exception $e) {
+            //response
+            throw new RestException(Api::STAT_APP_EXCEPTION, $e->getMessage());
+        }
+        return $response;
+    }
+
+    /**
+     * @param string $prjUid {@min 32} {@max 32}
+     *
+     * @url GET /:prjUid/available-inputdocument-supervisor
+     */
+    public function doGetAvailableInputDocumentSupervisor($prjUid)
+
+    {
+        try {
+            $supervisor = new \BusinessModel\ProcessSupervisor();
+            $arrayData = $supervisor->getAvailableInputDocumentSupervisor($prjUid);
+            //Response
+            $response = $arrayData;
+        } catch (\Exception $e) {
+            //response
+            throw new RestException(Api::STAT_APP_EXCEPTION, $e->getMessage());
+        }
+        return $response;
+    }
+
 
     /**
      * @url DELETE /:prjUid/supervisor/:supUid
