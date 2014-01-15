e1cf34526b90cd8e9c17f6f3e7bebbe15d140642
Erik Amaru Ortiz <erik@colosa.com>
2014-01-15 19:56:44 -0400

    Litle fix, adding user agent on curl call, for access token exchenge script.

--- workflow/engine/methods/oauth2/grant.php
+++ workflow/engine/methods/oauth2/grant.php
@@ -28,6 +28,7 @@ curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 
 $data = @json_decode(curl_exec($ch));
 curl_close($ch);
