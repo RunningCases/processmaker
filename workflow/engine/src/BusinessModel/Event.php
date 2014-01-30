1bada650fa4ea799f1d15c9ba4e0fdc72a674eb9
Brayan Osmar Pereyra Suxo <brayan@colosa.com>
2014-01-30 16:20:03 -0400

    Arreglo y correcion del api para Events

--- workflow/engine/src/BusinessModel/Event.php
+++ workflow/engine/src/BusinessModel/Event.php
@@ -93,14 +93,28 @@ class Event
         if ( ($sProcessUID == '') || (count($dataEvent) == 0) ) {
             return false;
         }
-
-        $oProcess = new \Process();
-        if (!($oProcess->processExists($sProcessUID))) {
-            throw (new \Exception( 'This process doesn\'t exist!' ));
-        }
-
         $dataEvent = array_change_key_case($dataEvent, CASE_UPPER);
 
+        $this->validateProcess($sProcessUID);
+
+        if ($dataEvent['EVN_RELATED_TO'] == 'SINGLE') {
+            if (empty($dataEvent['TAS_UID'])) {
+                throw (new \Exception('The field "tas_uid" is required!'));
+            }
+            $this->validateTask($dataEvent['TAS_UID']);
+        } else {
+            if (empty($dataEvent['EVN_TAS_UID_FROM'])) {
+                throw (new \Exception('The field "evn_tas_uid_from" is required!'));
+            }
+            $this->validateTask($dataEvent['EVN_TAS_UID_FROM']);
+
+            if (empty($dataEvent['EVN_TAS_UID_TO'])) {
+                throw (new \Exception('The field "evn_tas_uid_to" is required!'));
+            }
+            $this->validateTask($dataEvent['EVN_TAS_UID_TO']);
+        }
+
+        $this->validateTrigger($dataEvent['TRI_UID']);
         if ( $create && (isset($dataEvent['ENV_UID'])) ) {
             unset($dataEvent['ENV_UID']);
         }
@@ -138,6 +152,48 @@ class Event
         } catch (Exception $e) {
             throw $e;
         }
+    }
+
+    public function validateProcess($proUid) {
+        $proUid = trim($proUid);
+        if ($proUid == '') {
+            throw (new \Exception('This process doesn\'t exist!'));
+        }
+
+        $oProcess = new \Process();
+        if (!($oProcess->processExists($proUid))) {
+            throw (new \Exception('This process doesn\'t exist!'));
+        }
+
+        return $proUid;
+    }
+
+    public function validateTask($taskUid) {
+        $taskUid = trim($taskUid);
+        if ($taskUid == '') {
+            throw (new \Exception('This task doesn\'t exist!'));
+        }
+
+        $oTask = new \Task();
+        if (!($oTask->taskExists($taskUid))) {
+            throw (new \Exception('This task doesn\'t exist!'));
+        }
+
+        return $taskUid;
+    }
+
+    public function validateTrigger($triUid) {
+        $triUid = trim($triUid);
+        if ($triUid == '') {
+            throw (new \Exception('This trigger doesn\'t exist!'));
+        }
+
+        $oTriggers = new \Triggers();
+        if (!($oTriggers->TriggerExists($triUid))) {
+            throw (new \Exception('This trigger doesn\'t exist!'));
+        }
+
+        return $triUid;
     }
 }
 
