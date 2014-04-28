2e7f69edd305d8f335781db6d615ae823f430a6b
Daniel Rojas <daniel.rojas@colosa.com>
2014-04-28 17:15:27 -0400

    Se modifica la funcionalidad de start event para Case Scheduler y Web Entry (remove  flow)

--- workflow/engine/src/ProcessMaker/Project/Adapter/BpmnWorkflow.php
+++ workflow/engine/src/ProcessMaker/Project/Adapter/BpmnWorkflow.php
@@ -283,6 +283,54 @@ class BpmnWorkflow extends Project\Bpmn
                     $this->wp->setStartTask($activity->getActUid(), false);
                 }
             }
+
+            // update case scheduler
+            if (! is_null($event) && $event->getEvnType() == "START" && $event->getEvnMarker() == "TIMER") {
+                $aData = array(
+                    'SCH_NAME'=>'',
+                    'SCH_DEL_USER_NAME'=>'',
+                    'SCH_DEL_USER_UID'=>'',
+                    'TAS_UID'=>'',
+                    'SCH_LAST_RUN_TIME'=>NULL,
+                    'SCH_STATE'=>'',
+                    'SCH_LAST_STATE'=>'',
+                    'USR_UID'=>'',
+                    'SCH_OPTION'=>'',
+                    'SCH_DAYS_PERFORM_TASK'=>'',
+                    'SCH_EVERY_DAYS'=>NULL,
+                    'SCH_WEEK_DAYS'=>'',
+                    'SCH_START_DAY'=>'',
+                    'SCH_START_DAY_OPT_1'=>'',
+                    'SCH_START_DAY_OPT_2'=>'',
+                    'SCH_MONTHS'=>'',
+                    'SCH_REPEAT_EVERY'=>'',
+                    'SCH_REPEAT_STOP_IF_RUNNING'=>'',
+                    'CASE_SH_PLUGIN_UID'=>NULL,
+                    'SCH_DEL_USER_PASS'=>'',
+                    'SCH_UID'=>$flow->getFloElementOrigin(),
+                    'SCH_REPEAT_UNTIL'=>''
+                );
+                $this->wp->updateCaseScheduler($aData);
+            }
+
+            // update web entry
+            if (! is_null($event) && $event->getEvnType() == "START" && $event->getEvnMarker() == "MESSAGE") {
+                $aData = array(
+                    'WE_UID'=>$flow->getFloElementOrigin(),
+                    'TAS_UID'=>'',
+                    'DYN_UID'=>'',
+                    'TAS_UID'=>'',
+                    'USR_UID'=>'',
+                    'WE_METHOD'=>'',
+                    'WE_INPUT_DOCUMENT_ACCESS'=>'',
+                    'WE_DATA'=>'',
+                    'WE_CREATE_USR_UID'=>'',
+                    'WE_UPDATE_USR_UID'=>'',
+                    'WE_UPDATE_DATE'=>date('Y-m-d H:i:s')
+                );
+                $this->wp->updateWebEntry($aData);
+            }
+
         } elseif ($flow->getFloElementOriginType() == "bpmnActivity" &&
             $flow->getFloElementDestType() == "bpmnEvent") {
             // verify case: activity -> event(end)
