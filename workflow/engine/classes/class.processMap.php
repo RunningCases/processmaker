16566bcde8ec969f511b46c98d68c003c351f2b2
Daniel Rojas <daniel.rojas@colosa.com>
2014-05-02 16:25:13 -0400

    Bug 0014740: Fix. Se retiran los grupos que no contengan usuarios con el permiso PM_SUPERVISOR de la lista de asignación de los supervisores. Solo para Backend.

--- workflow/engine/classes/class.processMap.php
+++ workflow/engine/classes/class.processMap.php
@@ -3417,20 +3417,10 @@ class processMap
         $oDataset->next();
 
         while ($aRow = $oDataset->getRow()) {
-            $group = new ProcessMaker\BusinessModel\Group();
-            $userGroup = $group->getUsers('USERS', $aRow['GRP_UID']);
-            foreach ($userGroup as $value) {
-                $userRole = new ProcessMaker\BusinessModel\User();
-                $permission = $userRole->loadUserRolePermission('PROCESSMAKER', $value["USR_UID"]);
-                foreach ($permission as $values) {
-                    if ($values["PER_CODE"] == 'PM_SUPERVISOR') {
-                        $aRespLi[] = array('UID' => $aRow['GRP_UID'],
-                            'USER_GROUP' => $aRow['GRP_TITLE'],
-                            'TYPE_UID' => 'Group',
-                            'PRO_UID' => $sProcessUID);
-                    }
-                }
-            }
+            $aRespLi[] = array('UID' => $aRow['GRP_UID'],
+                'USER_GROUP' => $aRow['GRP_TITLE'],
+                'TYPE_UID' => 'Group',
+                'PRO_UID' => $sProcessUID);
             $oDataset->next();
         }
 
