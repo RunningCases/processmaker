78510d71d38c5ff99d5f2aeaf115053affa5f332
Roly Rudy Gutierrez Pinto <gproly@gmail.com>
2015-01-23 17:25:13 -0400

    IMPROVEMENT PM-1355

--- workflow/engine/methods/cases/ajaxListener.php
+++ workflow/engine/methods/cases/ajaxListener.php
@@ -851,7 +851,15 @@ class Ajax
         $Fields["APP_DATA"]["__DYNAFORM_OPTIONS"]["NEXT_STEP_LABEL"] = "";
         $Fields["APP_DATA"]["__DYNAFORM_OPTIONS"]["NEXT_STEP"] = "#";
         $Fields["APP_DATA"]["__DYNAFORM_OPTIONS"]["NEXT_ACTION"] = "return false;";
+        G::LoadClass('pmDynaform');
+        $a = new pmDynaform($_REQUEST['DYN_UID'], $Fields['APP_DATA']);
+        if ($a->isResponsive()) {
+            $a->app_data["PROCESS"] = $_SESSION['PROCESS'];
+            $a->app_data["SYS_SYS"] = SYS_SYS;
+            $a->printView((!isset($_SESSION["PM_RUN_OUTSIDE_MAIN_APP"])) ? "true" : "false", $_SESSION['APPLICATION']);
+        } else {
         $G_PUBLISH->AddContent("dynaform", "xmlform", $_SESSION["PROCESS"] . "/" . $_POST["DYN_UID"], "", $Fields["APP_DATA"], "", "", "view");
+        }
         ?>
 
         <script type="text/javascript">
