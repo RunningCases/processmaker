7a7cb06a817b48bac13df10c526da65f48d048fa
Freddy Daniel Rojas Valda <daniel.rojas@colosa.com>
2015-02-05 10:30:19 -0400

    PM-1436 "0016560: Dynaform conditions don't work on checkboxes when dynaform is in view mode" SOLVED

--- gulliver/system/class.xmlform.php
+++ gulliver/system/class.xmlform.php
@@ -3223,7 +3223,7 @@ class XmlForm_Field_Checkbox extends XmlForm_Field
                 $html = "<input value='{$this->value}' type='checkbox' $checked $readOnly disabled />
                  <span class='FormCheck'>" . $this->label . '</span></input>';
             } else {
-                $html = "<input value='{$this->value}' type='checkbox' $checked $readOnly disabled />";
+                $html = "<input id='form[" . $this->name . "]' value='{$this->value}' type='checkbox' $checked $readOnly disabled />";
             }
             $html .= "<input id='form[" . $this->name . "]' name='form[" . $this->name . "]' type='hidden' " . $this->NSFieldType() . " value='{$value}' />";
             return $html;
