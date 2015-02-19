c6589b7e174264bbcbefbd2edee1e6a4748f47f9
Paula V. Quispe <paula.quispe@colosa.com>
2015-02-19 14:31:48 -0400

    I solved the issue PM-1519

--- workflow/engine/classes/class.derivation.php
+++ workflow/engine/classes/class.derivation.php
@@ -830,8 +830,16 @@ class Derivation
             foreach ($aFields as $sOriginField => $sTargetField) {
                 $sOriginField = str_replace( '@', '', $sOriginField );
                 $sOriginField = str_replace( '#', '', $sOriginField );
+                $sOriginField = str_replace( '%', '', $sOriginField );
+                $sOriginField = str_replace( '?', '', $sOriginField );
+                $sOriginField = str_replace( '$', '', $sOriginField );
+                $sOriginField = str_replace( '=', '', $sOriginField );
                 $sTargetField = str_replace( '@', '', $sTargetField );
                 $sTargetField = str_replace( '#', '', $sTargetField );
+                $sTargetField = str_replace( '%', '', $sTargetField );
+                $sTargetField = str_replace( '?', '', $sTargetField );
+                $sTargetField = str_replace( '$', '', $sTargetField );
+                $sTargetField = str_replace( '=', '', $sTargetField );
                 $aNewFields[$sTargetField] = isset( $appFields['APP_DATA'][$sOriginField] ) ? $appFields['APP_DATA'][$sOriginField] : '';
             }
 
@@ -917,8 +925,16 @@ class Derivation
                 foreach ($aFields as $sOriginField => $sTargetField) {
                     $sOriginField = str_replace( '@', '', $sOriginField );
                     $sOriginField = str_replace( '#', '', $sOriginField );
+                    $sOriginField = str_replace( '%', '', $sOriginField );
+                    $sOriginField = str_replace( '?', '', $sOriginField );
+                    $sOriginField = str_replace( '$', '', $sOriginField );
+                    $sOriginField = str_replace( '=', '', $sOriginField );
                     $sTargetField = str_replace( '@', '', $sTargetField );
                     $sTargetField = str_replace( '#', '', $sTargetField );
+                    $sTargetField = str_replace( '%', '', $sTargetField );
+                    $sTargetField = str_replace( '?', '', $sTargetField );
+                    $sTargetField = str_replace( '$', '', $sTargetField );
+                    $sTargetField = str_replace( '=', '', $sTargetField );
                     $aNewFields[$sTargetField] = isset( $appFields['APP_DATA'][$sOriginField] ) ? $appFields['APP_DATA'][$sOriginField] : '';
                 }
                 $aParentCase['APP_DATA'] = array_merge( $aParentCase['APP_DATA'], $aNewFields );
