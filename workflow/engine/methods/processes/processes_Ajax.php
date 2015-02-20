f851220857e82cba6adb01702d26ad74495b0ee5
jonathan <jonathan.quispe@colosa.com>
2015-02-20 15:01:04 -0400

    add validate isset in file processes_Ajax.php

--- workflow/engine/methods/processes/processes_Ajax.php
+++ workflow/engine/methods/processes/processes_Ajax.php
@@ -117,7 +117,7 @@ try {
         $resultProcess = $infoProcess->getProcessRow($proUid);     
     }
     
-    if($proUid != "") {
+    if(isset($proUid) && $proUid != "") {
         $valuesProcess['PRO_UID'] = $proUid;
         $valuesProcess['PRO_UPDATE_DATE'] = date("Y-m-d H:m:i");
         G::LoadClass('processes');
