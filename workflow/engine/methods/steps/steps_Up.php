16d752d2927dd3320125ee1add5ea06b05cfde1a
Julio Cesar Laura <juliocesar@colosa.com>
2015-02-20 16:34:32 -0400

    IMPROVEMENT for the issue PM-1472

--- workflow/engine/methods/steps/steps_Up.php
+++ workflow/engine/methods/steps/steps_Up.php
@@ -38,7 +38,7 @@ try {
     require_once 'classes/model/Step.php';
     $oStep = new Step();
     $oStep->up( $_POST['STEP_UID'], $_POST['TASK'], $_POST['STEP_POSITION'] );
-    G::auditlog("OptionsMenuTask","Up the Step One Level -> ".$_POST['STEP_UID'].' In Task -> '.$_POST['TASK'].' Step Position -> '.$_POST['STEP_POSITION']);
+    G::auditlog("StepUp","Up the Step One Level -> ".$_POST['STEP_UID'].' In Task -> '.$_POST['TASK'].' Step Position -> '.$_POST['STEP_POSITION']);
     G::LoadClass( 'processMap' );
     $oProcessMap = new ProcessMap();
     $oProcessMap->getStepsCriteria( $_POST['TASK'] );
