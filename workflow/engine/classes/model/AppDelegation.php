d136eb9f169c4cbc0395c1bdb8865fbcd7858be4
Marco Antonio Nina Mena <marcoantonina@gmail.com>
2015-03-31 14:52:36 -0400

    -Improvement file appDelegation
        - quit line deprecated.

--- workflow/engine/classes/model/AppDelegation.php
+++ workflow/engine/classes/model/AppDelegation.php
@@ -153,8 +153,6 @@ class AppDelegation extends BaseAppDelegation
         if ($this->validate()) {
             try {
                 $res = $this->save();
-                //$inbox = new ListInbox();
-                //$inbox->newRow($this->toArray(BasePeer::TYPE_FIELDNAME), $delPreviusUsrUid);
             } catch (PropelException $e) {
                 throw ($e);
             }
