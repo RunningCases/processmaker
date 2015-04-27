17f1eb54b2bd6c88513eea3e6b3e784c129a5bd8
dheeyi <dheeyi@gmail.com>
2015-04-27 17:11:19 -0400

    PM-2394 Se muestra un "warning php" en "web entry" cuando dynaform esta vacio. Causa : Esto es fue ocacionado bajo el siguiente contexto. Si se crea un dynaform; sin embargo este no posee ningun item(Labels, titles,radio buttons...) en la base de datos DYNAFORM existe una columna de nombre "dyn_content" el cual almacena estes parametros, pero como no tiene ningun elemento este inicialmentre contendra como valor null. Solució Cuando se hace la definicion del DYNAFORM, paralelamente se adiciona un parametro por defecto(json vacio) a la columna "dyn_content". Y asi al generar el WEB ENTRY, al momento de renderear el dynaform se mostrara una pantalla vacia, limpia sin componentes y asi evitar el error PHP que se tenia, ya que se pretendia leer parametros cuyos en principio eran nulos.

--- workflow/engine/src/ProcessMaker/Services/Api/Project/DynaForm.php
+++ workflow/engine/src/ProcessMaker/Services/Api/Project/DynaForm.php
@@ -47,7 +47,9 @@ class DynaForm extends Api
             $dynaForm->setArrayFieldNameForException(array("processUid" => "prj_uid"));
 
             $arrayData = $dynaForm->executeCreate($prj_uid, $request_data);
-            $request_data['dyn_content']="{}";
+            if (!array_key_exists('dyn_content', $request_data)) {
+                $request_data['dyn_content']="{}";
+            }
             $arrayData = $dynaForm->update($arrayData['dyn_uid'], $request_data);
 
             $response = $arrayData;
