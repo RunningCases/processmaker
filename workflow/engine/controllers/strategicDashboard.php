b4e0a2cd6ed4dc234deb3741c1c9ed30f2647c31
Marco Antonio Nina Mena <marcoantonina@gmail.com>
2015-05-07 11:15:16 -0400

    Add validation access users

--- workflow/engine/controllers/strategicDashboard.php
+++ workflow/engine/controllers/strategicDashboard.php
@@ -21,11 +21,6 @@ class StrategicDashboard extends Controller
     {
         global $RBAC;
 
-        if ($RBAC->userCanAccess('PM_SETUP') != 1) {
-            G::SendTemporalMessage('ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels');
-            G::header( 'location: login/login' );
-            exit(0);
-        }
         $licensedFeatures = & PMLicensedFeatures::getSingleton();
         if (!$licensedFeatures->verifyfeature('r19Vm5DK1UrT09MenlLYjZxejlhNUZ1b1NhV0JHWjBsZEJ6dnpJa3dTeWVLVT0=')) {
             G::SendTemporalMessage( 'ID_USER_HAVENT_RIGHTS_PAGE', 'error', 'labels' );
