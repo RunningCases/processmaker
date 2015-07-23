3d8538da1e5c5fcd5ecf0b9b9afa6489c2969a95
dheeyi <dheeyi@gmail.com>
2015-07-23 14:16:13 -0400

    0017842: No aparecen varios usuarios para la asignacion

--- workflow/engine/src/ProcessMaker/BusinessModel/Task.php
+++ workflow/engine/src/ProcessMaker/BusinessModel/Task.php
@@ -1003,7 +1003,7 @@ class Task
                     }
                 }
             } else {
-                $limit = count($aUsers)+1;
+                $limit = count($aUsers) + 1;
             }
             $aUsers = $this->arrayPagination($aUsers, $start, $limit);
             return $aUsers;
