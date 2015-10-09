444539ee76520a4e39905993fd94f1810b9aed8a
Ronald Quenta <ronald.quenta@colosa.com>
2015-10-09 14:09:18 -0400

    PM-3755 Consume the oauth/token endpoint specifying an invalid workspace,

--- workflow/engine/src/ProcessMaker/Services/OAuth2/Server.php
+++ workflow/engine/src/ProcessMaker/Services/OAuth2/Server.php
@@ -392,23 +392,5 @@ class Server implements iAuthenticate
         return "";
     }
 
-    public static function loadPostEnvironment()
-    {
-        $acceptLanguage = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-        if (!defined('SYS_LANG')) {
-            $Translations = new \Translation;
-            $translationsTable = $Translations->getTranslationEnvironments();
-            $inLang = false;
-            foreach ($translationsTable as $locale) {
-                if ($locale['LOCALE'] == $acceptLanguage){
-                    $inLang = true;
-                    break;
-                }
-            }
-            $lang = $inLang?$acceptLanguage:'en';
-            define("SYS_LANG", $lang);
-        }
-
-    }
 }
 
