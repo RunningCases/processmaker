9c1b150c0e6128e6da7d69b2563709430e6b586e
Paula V. Quispe <paula.quispe@colosa.com>
2015-10-23 12:28:24 -0400

    EX-85

--- workflow/engine/src/ProcessMaker/Project/Adapter/BpmnWorkflow.php
+++ workflow/engine/src/ProcessMaker/Project/Adapter/BpmnWorkflow.php
@@ -233,6 +233,16 @@ class BpmnWorkflow extends Project\Bpmn
             $taskData["TAS_ASSIGN_TYPE"] = "BALANCED";
         }
 
+        if ($activityBefore->getActTaskType() == "SCRIPTTASK" && $activityCurrent->getActTaskType() == "SCRIPTTASK") {
+            $taskData["TAS_TYPE"] = "SCRIPT-TASK";
+            $taskData["TAS_ASSIGN_TYPE"] = "BALANCED";
+        }
+
+        if ($activityBefore->getActTaskType() == "EMPTY" && $activityCurrent->getActTaskType() == "EMPTY") {
+            $taskData["TAS_TYPE"] = "NORMAL";
+            $taskData["TAS_ASSIGN_TYPE"] = "BALANCED";
+        }
+
         if ($activityBefore->getActTaskType() == "SCRIPTTASK" && $activityCurrent->getActTaskType() != "SCRIPTTASK") {
             $taskData["TAS_TYPE"] = "NORMAL";
             $taskData["TAS_ASSIGN_TYPE"] = "BALANCED";
