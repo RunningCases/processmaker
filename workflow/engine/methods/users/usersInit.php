8e13886b73a04afae34dbb40dfe9590dba2bb1e6
Paula V. Quispe <paula.quispe@colosa.com>
2015-12-03 15:01:58 -0400

    PM-4256

--- workflow/engine/methods/users/usersInit.php
+++ workflow/engine/methods/users/usersInit.php
@@ -26,6 +26,13 @@ $uploadMaxSize = (int) $UPLOAD_MAX_SIZE * $mul;
 if ($postMaxSize < $uploadMaxSize) {
     $uploadMaxSize = $postMaxSize;
 }
+$languageManagement = 0;
+/*----------------------------------********---------------------------------*/
+$licensedFeatures = & PMLicensedFeatures::getSingleton();
+if ($licensedFeatures->verifyfeature('w2LL3o4NFNiaDRXcFFCYVpJS3Jsall5dmh0ZWtBTkdKR3ROS0VzWGdoLzNQYz0=')) {
+    $languageManagement = 1;
+}
+/*----------------------------------********---------------------------------*/
 
 $arraySystemConfiguration = System::getSystemConfiguration('', '', SYS_SYS);
 
@@ -41,5 +48,6 @@ $oHeadPublisher->assign( 'MODE', '' );
 $oHeadPublisher->assign('SYSTEM_TIME_ZONE', $arraySystemConfiguration['time_zone']);
 $oHeadPublisher->assign('TIME_ZONE_DATA', array_map(function ($value) { return [$value, $value]; }, DateTimeZone::listIdentifiers()));
 $oHeadPublisher->assign('__SYSTEM_UTC_TIME_ZONE__', (isset($_SESSION['__SYSTEM_UTC_TIME_ZONE__']) && $_SESSION['__SYSTEM_UTC_TIME_ZONE__'])? 1 : 0);
+$oHeadPublisher->assign('LANGUAGE_MANAGEMENT', $languageManagement);
 
 G::RenderPage( 'publish', 'extJs' );
