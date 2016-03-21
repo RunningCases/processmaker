8e85c799ec58d9f91b300729a00b09185851c668
mcuiza <marcelo.cuiza@processmaker.com>
2016-03-21 17:04:22 -0400

    export

--- workflow/engine/src/ProcessMaker/Services/Api/Project.php
+++ workflow/engine/src/ProcessMaker/Services/Api/Project.php
@@ -145,7 +145,7 @@ class Project extends Api
     {
         $objects = \G::json_decode($objects);
         $granularExporter = new GranularExporter($prj_uid);
-        $outputFilename = $granularExporter->export($objects->objectList);
+        $outputFilename = $granularExporter->export($objects);
 
         $httpStream = new HttpStream();
         $fileExtension = pathinfo($outputFilename, PATHINFO_EXTENSION);
