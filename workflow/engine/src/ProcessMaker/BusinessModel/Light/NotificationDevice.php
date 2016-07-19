d1d15fe342945535e09c25f6a2610b33d42eb952
dheeyi william <dheeyi@gmail.com>
2016-07-19 10:40:08 -0400

    HOR-1417 caseNumber needed on notificatiosn mobile

--- workflow/engine/src/ProcessMaker/BusinessModel/Light/NotificationDevice.php
+++ workflow/engine/src/ProcessMaker/BusinessModel/Light/NotificationDevice.php
@@ -201,7 +201,8 @@ class NotificationDevice
                     'caseId' => $appFields['APP_UID'],
                     'caseTitle' => $appFields['APP_TITLE'],
                     'delIndex' => $delIndex,
-                    'typeList' => $typeList
+                    'typeList' => $typeList,
+                    'caseNumber' => $appFields['APP_NUMBER']
                 );
 
                 if ($userIds) {
