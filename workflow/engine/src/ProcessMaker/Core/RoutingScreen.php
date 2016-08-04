8f2ce6cc81525a9d57bce9a1da6dd2194b33f323
Paula V. Quispe <paula.quispe@colosa.com>
2016-08-04 12:00:42 -0400

    HOR-1502

--- workflow/engine/src/ProcessMaker/Core/RoutingScreen.php
+++ workflow/engine/src/ProcessMaker/Core/RoutingScreen.php
@@ -13,31 +13,22 @@ class RoutingScreen extends \Derivation
 
     public function mergeDataDerivation($post, $prepareInformation)
     {
-        $count = 1;
         $iPost = count($post);
         $aDataMerged = array();
-        $flag = false;
+        $flagJumpTask = false;
         foreach ($prepareInformation as $key => $nextTask) {
+            $aDataMerged[$key] = $nextTask['NEXT_TASK'];
+            unset($aDataMerged[$key]['USER_ASSIGNED']);
+            $aDataMerged[$key]['DEL_PRIORITY'] = '';
             for ($i = 1; $i <= $iPost; $i++) {
-                if(isset($post[$i]['SOURCE_UID']) && $nextTask['NEXT_TASK']['TAS_UID'] === $post[$i]['SOURCE_UID']){
-                    if($post[$i]['SOURCE_UID'] !== $post[$i]['TAS_UID']){
-                        $aDataMerged[$count] = $nextTask['NEXT_TASK'];
-                        unset($aDataMerged[$count]['USER_ASSIGNED']);
-                        $aDataMerged[$count]['DEL_PRIORITY'] = '';
-                        $aDataMerged[$count]['NEXT_ROUTING'] = $post[$i];
-                        $count++;
-                        $flag = true;
-                    } else {
-                        $aDataMerged[$count] = \G::array_merges($nextTask['NEXT_TASK'],$post[$i]);
-                        unset($aDataMerged[$count]['USER_ASSIGNED']);
-                        $count++;
-                        $flag = true;
-                        break;
-                    }
+                if(isset($post[$i]['SOURCE_UID']) && ($nextTask['NEXT_TASK']['TAS_UID'] === $post[$i]['SOURCE_UID']) && ($post[$i]['SOURCE_UID'] !== $post[$i]['TAS_UID'])){
+                    $flagJumpTask = true;
+                    $aDataMerged[$key]['NEXT_ROUTING'][] = $post[$i];
                 }
             }
         }
-        if(!$flag){
+        //If flagJumpTask is false the template does not Jump Intermediate Events
+        if(!$flagJumpTask){
             $aDataMerged = $post;
         }
         return $aDataMerged;
