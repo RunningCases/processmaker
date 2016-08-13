a5ac2ec3fc86c7ceabc33731631c53eebfe37684
Julio Cesar Laura Avendaño <juliocesar@processmaker.com>
2016-08-13 10:05:01 -0400

    Add missing APP_SEQUENCE record

--- workflow/engine/data/mysql/insert.sql
+++ workflow/engine/data/mysql/insert.sql
@@ -61555,5 +61555,6 @@ INSERT INTO ADDONS_MANAGER (ADDON_DESCRIPTION,ADDON_ID,ADDON_NAME,ADDON_NICK,ADD
 ('User-based Language Management.','userBasedLanguage','userBasedLanguage','userBasedLanguage','Colosa','localRegistry','ready','00000000000000000000000000010013','features','','','0'),
 ('User-based Time Zone Management.','userBasedTimeZone','userBasedTimeZone','userBasedTimeZone','Colosa','localRegistry','ready','00000000000000000000000000010014','features','','','0'),
 ('This Feature will allow to store all input, output and attached documents generated in your processes in Google Drive.','pmGoogleDrive','pmGoogleDrive','pmGoogleDrive','Colosa','localRegistry','ready','00000000000000000000000000010015','features','','','0'),
-('Promotion Manager','selectiveImportExport','selectiveImportExport','selectiveImportExport','Colosa','localRegistry','ready','00000000000000000000000000010016','features','','','0')
-;
+('Promotion Manager','selectiveImportExport','selectiveImportExport','selectiveImportExport','Colosa','localRegistry','ready','00000000000000000000000000010016','features','','','0');
+
+INSERT INTO APP_SEQUENCE (ID) VALUES ('0');
