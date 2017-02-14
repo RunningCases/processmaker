3e0392bfa7adcb753f57aaf0380a0c52e462690e
Chloe Deguzman <chloe.deguzman@processmaker.com>
2017-02-14 17:29:08 +0000

    HOR-2689 Removed LoadClassRBAC references from testAuthenticationUser.php and rbac.appEdit.php

--- workflow/engine/templates/testAuthenticationUser.php
+++ workflow/engine/templates/testAuthenticationUser.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * testAuthenticationUser.php
- *  
+ *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2008 Colosa Inc.23
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -14,13 +14,13 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd., 
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- * 
+ *
  */
 
   $strPass = $_POST['form'][PASS];
@@ -39,13 +39,11 @@
   $dbc = new DBConnection(DB_HOST, DB_RBAC_USER, DB_RBAC_PASS, DB_RBAC_NAME );
 
   //Class user
-  G::LoadClassRBAC ("user");
   $user = new RBAC_User;
   $user->SetTo ($dbc);
   $user->Load ($userId);
 
   //crear nueva authentication source
-  G::LoadClassRBAC ('authentication');
   $obj = new authenticationSource;
   $obj->SetTo( $dbc );
   $res = $obj->verifyPassword ( $userId, $user->Fields['USR_LDAP_DN'] , $strPass,  $user->Fields['USR_LDAP_SOURCE'] );
