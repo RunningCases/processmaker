f17abb3f56cc9ec85502d104002a7fbe9abbbcd6
davidcallizaya <david.callizaya@processmaker.com>
2017-02-16 16:40:59 -0400

    - Remove license text

--- workflow/engine/bin/tasks/cliListIds.php
+++ workflow/engine/bin/tasks/cliListIds.php
@@ -1,29 +1,4 @@
 <?php
-/**
- * cliUpgrade.php
- *
- * ProcessMaker Open Source Edition
- * Copyright (C) 2011 Colosa Inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
- * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
- * @author Alexandre Rosenfeld <alexandre@colosa.com>
- * @package workflow-engine-bin-tasks
- */
 CLI::taskName('list-ids');
 CLI::taskDescription(<<<EOT
     Complete the PRO_ID and USR_ID in the LIST_* tables.
