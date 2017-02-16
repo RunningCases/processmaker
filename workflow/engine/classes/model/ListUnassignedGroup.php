c2278063b47b82bd01b67651f2d1b2b9f136ba95
davidcallizaya <david.callizaya@processmaker.com>
2017-02-16 14:29:52 -0400

    + Add ListUnassignedGroup

--- workflow/engine/classes/model/ListUnassignedGroup.php
+++ workflow/engine/classes/model/ListUnassignedGroup.php
@@ -24,6 +24,8 @@ class ListUnassignedGroup extends BaseListUnassignedGroup {
      */
     public function create($data)
     { 
+        $u = new Users();
+        if(!empty($data['USR_UID'])) $data['USR_ID'] = $data['USR_UID']==='SELF_SERVICES' ? null : $u->load($data['USR_UID'])['USR_ID'];
         $con = Propel::getConnection( ListUnassignedGroupPeer::DATABASE_NAME );
         try {
             $this->fromArray( $data, BasePeer::TYPE_FIELDNAME );
