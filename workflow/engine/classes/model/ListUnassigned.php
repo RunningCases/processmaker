c58a08efe067906b0a7cd09e88509401ee4ea496
davidcallizaya <david.callizaya@processmaker.com>
2017-02-23 14:47:39 -0400

    + Add PRO_ID verification to avoid reload the process when not needed.

--- workflow/engine/classes/model/ListUnassigned.php
+++ workflow/engine/classes/model/ListUnassigned.php
@@ -28,7 +28,7 @@ class ListUnassigned extends BaseListUnassigned
      */
     public function create($data)
     {
-        if (!empty($data['PRO_UID'])) {
+        if (!empty($data['PRO_UID']) && empty($data['PRO_ID'])) {
             $p = new Process();
             $data['PRO_ID'] =  $p->load($data['PRO_UID'])['PRO_ID'];
         }
