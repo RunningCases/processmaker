7f065f167e9573795fcda3f41635cc5b28f30673
qronald <ronald.quenta@processmaker.com>
2017-05-29 08:47:29 -0400

    add comment

--- workflow/engine/src/ProcessMaker/Util/System.php
+++ workflow/engine/src/ProcessMaker/Util/System.php
@@ -28,6 +28,14 @@ class System
         }
     }
 
+    /**
+     * Returns workspace objects from an array of workspace names.
+     *
+     * @param array $args an array of workspace names
+     * @param bool $includeAll if true and no workspace is specified in args,
+     *                          returns all available workspaces
+     * @return array of workspace objects
+     */
     public static function getWorkspacesFromArgs($args, $includeAll = true)
     {
         $workspaces = array();
