e8b3ffb5c6f521170266a5693db1cc0f30a90eeb
Taylor Dondich <tdondich@Taylors-MacBook-Air.local>
2017-07-27 15:29:55 -0700

    Add the appropriate laravel bootstrapping to PM web application and command line tool.

--- workflow/public_html/app.php
+++ workflow/public_html/app.php
@@ -1,5 +1,9 @@
 <?php
-require_once(__DIR__ . '/../../bootstrap/autoload.php');
+// Because laravel has a __ helper function, it's important we include the class.g file to ensure our __ is used.
+require_once __DIR__.'/../../gulliver/system/class.g.php';
+require_once __DIR__.'/../../bootstrap/autoload.php';
+require_once __DIR__.'/../../bootstrap/app.php';
+
 register_shutdown_function(
     create_function(
         "",
