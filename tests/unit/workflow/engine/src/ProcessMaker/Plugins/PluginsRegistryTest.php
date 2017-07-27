d80ee5080ad3f72a3ade034f25661718889aafa3
Ronald Quenta <ronald.quenta@processmaker.com>
2017-07-27 16:32:07 -0400

    adding more test

--- tests/unit/workflow/engine/src/ProcessMaker/Plugins/PluginsRegistryTest.php
+++ tests/unit/workflow/engine/src/ProcessMaker/Plugins/PluginsRegistryTest.php
@@ -3,9 +3,12 @@
 namespace Tests\ProcessMaker\Plugins;
 
 use ProcessMaker\Plugins\Interfaces\CssFile;
+use ProcessMaker\Plugins\Interfaces\FolderDetail;
 use ProcessMaker\Plugins\Interfaces\MenuDetail;
 use ProcessMaker\Plugins\Interfaces\PluginDetail;
 use ProcessMaker\Plugins\Interfaces\Plugins;
+use ProcessMaker\Plugins\Interfaces\RedirectDetail;
+use ProcessMaker\Plugins\Interfaces\StepDetail;
 use ProcessMaker\Plugins\PluginsRegistry;
 use Tests\WorkflowTestCase;
 
@@ -320,11 +323,196 @@ class PluginsRegistryTest extends WorkflowTestCase
             [PATH_PLUGINS . 'js' . PATH_SEP . 'test2.js']
         );
         $js = $oPluginRegistry->getRegisteredJavascript();
-        $this->assertEquals(1, count($js));
+        $this->assertEquals(2, count($js));
         $this->assertObjectHasAttribute(
             'pluginJsFile',
             $js[0],
             'pluginJsFile attribute does not exist'
         );
+    }
+
+    /**
+     * Test get registered javascript by path
+     */
+    public function testGetRegisteredJavascriptBy()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin("enterprise");
+        $oPluginRegistry->registerJavascript(
+            'enterprise',
+            PATH_CORE . 'js' . PATH_SEP . 'testCore.js',
+            [PATH_PLUGINS . 'js' . PATH_SEP . 'test.js']
+        );
+        $js = $oPluginRegistry->getRegisteredJavascriptBy(PATH_CORE . 'js' . PATH_SEP . 'testCore.js');
+        $this->assertEquals(PATH_PLUGINS . 'js' . PATH_SEP . 'test.js', $js[0]);
+        $oPluginRegistry->getRegisteredJavascriptBy(PATH_CORE . 'js' . PATH_SEP . 'testCore.js', 'enterprise');
+    }
+
+    /**
+     * Test get unregistered javascript
+     */
+    public function testUnregisterJavascripts()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin("enterprise");
+        $oPluginRegistry->registerJavascript(
+            'enterprise',
+            PATH_CORE . 'js' . PATH_SEP . 'testCore.js',
+            [PATH_PLUGINS . 'js' . PATH_SEP . 'test.js']
+        );
+        $oPluginRegistry->unregisterJavascripts('enterprise');
+        $this->assertEquals(0, count($oPluginRegistry->_aJavascripts));
+        $oPluginRegistry->registerJavascript(
+            'enterprise',
+            PATH_CORE . 'js' . PATH_SEP . 'testCore.js',
+            [PATH_PLUGINS . 'js' . PATH_SEP . 'test.js']
+        );
+        $oPluginRegistry->unregisterJavascripts('enterprise', PATH_CORE . 'js' . PATH_SEP . 'testCore.js');
+    }
+
+    /**
+     * Test register report
+     */
+    public function testRegisterReport()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin("enterprise");
+        $oPluginRegistry->registerReport('enterprise');
+        $this->assertEquals(1, count($oPluginRegistry->_aReports));
+        $oPluginRegistry->registerReport('enterprise');
+    }
+
+    /**
+     * Test register pmfunction
+     */
+    public function testRegisterPmFunction()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin("enterprise");
+        $oPluginRegistry->registerPmFunction('enterprise');
+        $this->assertEquals(1, count($oPluginRegistry->_aPmFunctions));
+        $oPluginRegistry->registerPmFunction('enterprise');
+    }
+
+    /**
+     * Test registered redirect login
+     */
+    public function testRegisterRedirectLogin()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin("enterprise");
+        $oPluginRegistry->registerRedirectLogin('enterprise', 'TEST', PATH_CORE . 'js' . PATH_SEP . 'testCore.js');
+        $this->assertEquals(1, count($oPluginRegistry->_aRedirectLogin));
+        $this->assertInstanceOf(RedirectDetail::class, $oPluginRegistry->_aRedirectLogin[0], '');
+        $oPluginRegistry->registerRedirectLogin('enterprise', 'TEST', PATH_CORE . 'js' . PATH_SEP . 'testCore.js');
+    }
+
+    /**
+     * Test registered folder
+     */
+    public function testRegisterFolder()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin("enterprise");
+        $oPluginRegistry->registerFolder('enterprise', '1234', 'TestCore');
+        $this->assertEquals(2, count($oPluginRegistry->_aFolders));
+        $this->assertInstanceOf(FolderDetail::class, $oPluginRegistry->_aFolders[1], '');
+        $oPluginRegistry->registerFolder('enterprise', '1234', 'TestCore');
+    }
+
+    /**
+     * Test register step
+     */
+    public function testRegisterStep()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin("enterprise");
+        $oPluginRegistry->registerStep('enterprise', '1234', 'TestStep', 'StepTitle');
+        $this->assertEquals(1, count($oPluginRegistry->_aSteps));
+        $this->assertInstanceOf(StepDetail::class, $oPluginRegistry->_aSteps[0], '');
+        $oPluginRegistry->registerStep('enterprise', '1234', 'TestStep', 'StepTitle');
+    }
+
+    /**
+     * Test is registered folder
+     */
+    public function testIsRegisteredFolder()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin("enterprise");
+        $oPluginRegistry->registerFolder('charts', '1234', 'charts');
+        $step = $oPluginRegistry->isRegisteredFolder('charts');
+        $this->assertTrue($step);
+        $oPluginRegistry->registerFolder('pmosCommunity', '1234', 'config');
+        $step = $oPluginRegistry->isRegisteredFolder('config');
+        $this->assertEquals('pmosCommunity', $step);
+        $step = $oPluginRegistry->isRegisteredFolder('noExist');
+        $this->assertFalse($step);
+    }
+
+    /**
+     * Test get menus
+     */
+    public function testGetMenus()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin('enterprise');
+        $oPluginRegistry->registerMenu(
+            'charts',
+            'processmaker',
+            PATH_PLUGINS . 'charts.php'
+        );
+        $oPluginRegistry->getMenus('processmaker');
+        $this->assertTrue(file_exists($oPluginRegistry->_aMenus[0]->sFilename));
+    }
+
+    /**
+     * Test get dashlets
+     */
+    public function testGetDashlets()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin('enterprise');
+        $this->assertTrue(is_array($oPluginRegistry->getDashlets()));
+    }
+
+    /**
+     * Test get reports
+     */
+    public function testGetReports()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin('enterprise');
+        $this->assertTrue(is_array($oPluginRegistry->getReports()));
+    }
+
+    /**
+     * Test get pmfunction
+     */
+    public function testGetPmFunctions()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin('enterprise');
+        $this->assertTrue(is_array($oPluginRegistry->getPmFunctions()));
+    }
+
+    /**
+     * Test get steps
+     */
+    public function testGetSteps()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin('enterprise');
+        $this->assertTrue(is_array($oPluginRegistry->getSteps()));
+    }
+
+    /**
+     * Test get redirect login
+     */
+    public function testGetRedirectLogins()
+    {
+        $oPluginRegistry = PluginsRegistry::loadSingleton();
+        $oPluginRegistry->enablePlugin('enterprise');
+        $this->assertTrue(is_array($oPluginRegistry->getRedirectLogins()));
     }
 }
