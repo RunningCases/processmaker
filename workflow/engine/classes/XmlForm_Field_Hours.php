bf4132cc01fc9099d8794c3a1348cf201f0e71d5
David Callizaya <david.callizaya@processmaker.com>
2017-08-11 17:02:12 -0400

    remote: Compressing objects: 100% (28/28), done. HOR-3670 Removed non required comments from XmlForm_Field_hours

--- workflow/engine/classes/XmlForm_Field_Hours.php
+++ workflow/engine/classes/XmlForm_Field_Hours.php
@@ -1,36 +1,10 @@
 <?php
 
 /**
- * class.xmlfield_InputPM.php
- *
- * @package workflow.engine.classes
- *
- * ProcessMaker Open Source Edition
- * Copyright (C) 2004 - 2011 Colosa Inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
- * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- *
- * @package workflow.engine.classes
- **/
-
-
-/**
  * Class XmlForm_Field_hours
- */class XmlForm_Field_hours extends XmlForm_Field_SimpleText
+ *
+ */
+class XmlForm_Field_hours extends XmlForm_Field_SimpleText
 {
     public $size = 15;
     public $maxLength = 64;
@@ -45,7 +19,7 @@
     public $readOnly = false;
     public $sqlConnection = 0;
     public $sql = '';
-    public $sqlOption = array ();
+    public $sqlOption = array();
     //Atributes only for grids
     public $formula = '';
     public $function = '';
@@ -63,21 +37,21 @@
      * @param eter string owner
      * @return string
      */
-    public function render ($value = null, $owner = null)
+    public function render($value = null, $owner = null)
     {
         if ($this->strTo === 'UPPER') {
-            $value = strtoupper( $value );
+            $value = strtoupper($value);
         }
         if ($this->strTo === 'LOWER') {
-            $value = strtolower( $value );
+            $value = strtolower($value);
         }
             //if ($this->strTo==='CAPITALIZE') $value = strtocapitalize($value);
-        $onkeypress = G::replaceDataField( $this->onkeypress, $owner->values );
+        $onkeypress = G::replaceDataField($this->onkeypress, $owner->values);
         if ($this->replaceTags == 1) {
-            $value = G::replaceDataField( $value, $owner->values );
+            $value = G::replaceDataField($value, $owner->values);
         }
         if ($this->showVars == 1) {
-            $this->process = G::replaceDataField( $this->process, $owner->values );
+            $this->process = G::replaceDataField($this->process, $owner->values);
             //$sShowVars = '&nbsp;<a href="#" onclick="showDynaformsFormVars(\'form['.$this->name.']\', \'../controls/varsAjax\', \'' . $this->process . '\', \'' . $this->symbol . '\');return false;">' . $this->symbol . '</a>';
             $sShowVars = '&nbsp;<input type="button" value="' . $this->symbol . '" onclick="showDynaformsFormVars(\'form[' . $this->name . ']\', \'../controls/varsAjax\', \'' . $this->process . '\', \'' . $this->symbol . '\');return false;"/>';
         } else {
@@ -85,14 +59,14 @@
         }
         if ($this->mode === 'edit') {
             if ($this->readOnly) {
-                return '<input class="module_app_input___gray" id="form[' . $this->name . ']" name="form[' . $this->name . ']" type ="text" size="' . $this->size . '" maxlength="' . $this->maxLength . '" value=\'' . $this->htmlentities( $value, ENT_COMPAT, 'utf-8' ) . '\' readOnly="readOnly" style="' . htmlentities( $this->style, ENT_COMPAT, 'utf-8' ) . '" onkeypress="' . htmlentities( $onkeypress, ENT_COMPAT, 'utf-8' ) . '"/>' . $sShowVars;
+                return '<input class="module_app_input___gray" id="form[' . $this->name . ']" name="form[' . $this->name . ']" type ="text" size="' . $this->size . '" maxlength="' . $this->maxLength . '" value=\'' . $this->htmlentities($value, ENT_COMPAT, 'utf-8') . '\' readOnly="readOnly" style="' . htmlentities($this->style, ENT_COMPAT, 'utf-8') . '" onkeypress="' . htmlentities($onkeypress, ENT_COMPAT, 'utf-8') . '"/>' . $sShowVars;
             } else {
-                return '<input class="module_app_input___gray" id="form[' . $this->name . ']" name="form[' . $this->name . ']" type ="text" size="' . $this->size . '" maxlength="' . $this->maxLength . '" value=\'' . $this->htmlentities( $value, ENT_COMPAT, 'utf-8' ) . '\' style="' . htmlentities( $this->style, ENT_COMPAT, 'utf-8' ) . '" onkeypress="' . htmlentities( $onkeypress, ENT_COMPAT, 'utf-8' ) . '"/>' . $sShowVars;
+                return '<input class="module_app_input___gray" id="form[' . $this->name . ']" name="form[' . $this->name . ']" type ="text" size="' . $this->size . '" maxlength="' . $this->maxLength . '" value=\'' . $this->htmlentities($value, ENT_COMPAT, 'utf-8') . '\' style="' . htmlentities($this->style, ENT_COMPAT, 'utf-8') . '" onkeypress="' . htmlentities($onkeypress, ENT_COMPAT, 'utf-8') . '"/>' . $sShowVars;
             }
         } elseif ($this->mode === 'view') {
-            return '<input class="module_app_input___gray" id="form[' . $this->name . ']" name="form[' . $this->name . ']" type ="text" size="' . $this->size . '" maxlength="' . $this->maxLength . '" value=\'' . $this->htmlentities( $value, ENT_COMPAT, 'utf-8' ) . '\' style="display:none;' . htmlentities( $this->style, ENT_COMPAT, 'utf-8' ) . '" onkeypress="' . htmlentities( $onkeypress, ENT_COMPAT, 'utf-8' ) . '"/>' . $this->htmlentities( $value, ENT_COMPAT, 'utf-8' );
+            return '<input class="module_app_input___gray" id="form[' . $this->name . ']" name="form[' . $this->name . ']" type ="text" size="' . $this->size . '" maxlength="' . $this->maxLength . '" value=\'' . $this->htmlentities($value, ENT_COMPAT, 'utf-8') . '\' style="display:none;' . htmlentities($this->style, ENT_COMPAT, 'utf-8') . '" onkeypress="' . htmlentities($onkeypress, ENT_COMPAT, 'utf-8') . '"/>' . $this->htmlentities($value, ENT_COMPAT, 'utf-8');
         } else {
-            return $this->htmlentities( $value, ENT_COMPAT, 'utf-8' );
+            return $this->htmlentities($value, ENT_COMPAT, 'utf-8');
         }
     }
 
@@ -105,16 +79,16 @@
      * @param eter string owner
      * @return string
      */
-    public function renderGrid ($values = array(),$owner)
+    public function renderGrid($values = array(), $owner)
     {
-        $result = array ();
+        $result = array();
         $r = 1;
         foreach ($values as $v) {
             if ($this->replaceTags == 1) {
-                $v = G::replaceDataField( $v, $owner->values );
+                $v = G::replaceDataField($v, $owner->values);
             }
             if ($this->showVars == 1) {
-                $this->process = G::replaceDataField( $this->process, $owner->values );
+                $this->process = G::replaceDataField($this->process, $owner->values);
                 //$sShowVars = '&nbsp;<a href="#" onclick="showDynaformsFormVars(\'form['.$owner->name .']['.$r.']['.$this->name.']\', \'../controls/varsAjax\', \'' . $this->process . '\', \'' . $this->symbol . '\');return false;">' . $this->symbol . '</a>';
                 $sShowVars = '&nbsp;<input type="button" value="' . $this->symbol . '" onclick="showDynaformsFormVars(\'form[' . $owner->name . '][' . $r . '][' . $this->name . ']\', \'../controls/varsAjax\', \'' . $this->process . '\', \'' . $this->symbol . '\');return false;"/>';
             } else {
@@ -122,14 +96,14 @@
             }
             if ($this->mode === 'edit') {
                 if ($this->readOnly) {
-                    $result[] = '<input class="module_app_input___gray" id="form[' . $owner->name . '][' . $r . '][' . $this->name . ']" name="form[' . $owner->name . '][' . $r . '][' . $this->name . ']" type ="text" size="' . $this->size . '" maxlength="' . $this->maxLength . '" value="' . $this->htmlentities( $v, ENT_COMPAT, 'utf-8' ) . '" readOnly="readOnly" style="' . htmlentities( $this->style, ENT_COMPAT, 'utf-8' ) . '"/>' . $sShowVars;
+                    $result[] = '<input class="module_app_input___gray" id="form[' . $owner->name . '][' . $r . '][' . $this->name . ']" name="form[' . $owner->name . '][' . $r . '][' . $this->name . ']" type ="text" size="' . $this->size . '" maxlength="' . $this->maxLength . '" value="' . $this->htmlentities($v, ENT_COMPAT, 'utf-8') . '" readOnly="readOnly" style="' . htmlentities($this->style, ENT_COMPAT, 'utf-8') . '"/>' . $sShowVars;
                 } else {
-                    $result[] = '<input class="module_app_input___gray" id="form[' . $owner->name . '][' . $r . '][' . $this->name . ']" name="form[' . $owner->name . '][' . $r . '][' . $this->name . ']" type ="text" size="' . $this->size . '" maxlength="' . $this->maxLength . '" value="' . $this->htmlentities( $v, ENT_COMPAT, 'utf-8' ) . '" style="' . htmlentities( $this->style, ENT_COMPAT, 'utf-8' ) . '"/>' . $sShowVars;
+                    $result[] = '<input class="module_app_input___gray" id="form[' . $owner->name . '][' . $r . '][' . $this->name . ']" name="form[' . $owner->name . '][' . $r . '][' . $this->name . ']" type ="text" size="' . $this->size . '" maxlength="' . $this->maxLength . '" value="' . $this->htmlentities($v, ENT_COMPAT, 'utf-8') . '" style="' . htmlentities($this->style, ENT_COMPAT, 'utf-8') . '"/>' . $sShowVars;
                 }
             } elseif ($this->mode === 'view') {
-                $result[] = '<p align="' . $this->align . '">' . $this->htmlentities( number_format( $v, 2 ), ENT_COMPAT, 'utf-8' ) . '</p>';
+                $result[] = '<p align="' . $this->align . '">' . $this->htmlentities(number_format($v, 2), ENT_COMPAT, 'utf-8') . '</p>';
             } else {
-                $result[] = '<p align="' . $this->align . '">' . $this->htmlentities( number_format( $v, 2 ), ENT_COMPAT, 'utf-8' ) . '</p>';
+                $result[] = '<p align="' . $this->align . '">' . $this->htmlentities(number_format($v, 2), ENT_COMPAT, 'utf-8') . '</p>';
             }
             $r ++;
         }
@@ -143,7 +117,7 @@
      * @param eter string $element
      * @return string
      */
-    public function attachEvents ($element)
+    public function attachEvents($element)
     {
         return "myForm.aElements[i] = new G_Text(myForm, $element,'{$this->name}');
     myForm.aElements[i].setAttributes(" . $this->getAttributes() . ");";
