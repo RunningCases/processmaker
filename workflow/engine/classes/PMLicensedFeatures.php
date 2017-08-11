879c20f62199e6bda9075ebaf241d2025f68bf24
Marco A. Nina Mena <marco.antonio.nina@processmaker.coma>
2017-08-11 12:19:39 -0400

    files

--- workflow/engine/classes/PMLicensedFeatures.php
+++ workflow/engine/classes/PMLicensedFeatures.php
@@ -335,10 +335,6 @@ class PMLicensedFeatures
     /*----------------------------------********---------------------------------*/
     public function verifyfeature ($featureName)
     {
-        if (!class_exists("pmLicenseManager")) {
-            require_once ("classes" . PATH_SEP . "class.pmLicenseManager.php");
-        }
-
         $licenseManager = pmLicenseManager::getSingleton(false);
 
         $_SESSION['__sw__'] = true;
