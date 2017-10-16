5456397036cf04a8a5a92385f11eba4590fe0066
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2017-10-16 15:47:36 -0400

    Add realpath to variable File

--- workflow/engine/methods/processes/processes_GetFile.php
+++ workflow/engine/methods/processes/processes_GetFile.php
@@ -5,7 +5,7 @@ $RBAC->allows(basename(__FILE__), $_GET['MAIN_DIRECTORY']);
 $mainDirectory = !empty($_GET['MAIN_DIRECTORY']) ? $_GET['MAIN_DIRECTORY'] : '';
 $proUid = !empty($_GET['PRO_UID']) ? $_GET['PRO_UID'] : '';
 $currentDirectory = !empty($_GET['CURRENT_DIRECTORY']) ? realpath($_GET['CURRENT_DIRECTORY']) . PATH_SEP : '';
-$file = !empty($_GET['FILE']) ? $_GET['FILE'] : '';
+$file = !empty($_GET['FILE']) ? realpath($_GET['FILE']) : '';
 $extension = (!empty($_GET['sFilextension']) && $_GET['sFilextension'] === 'javascript') ? '.js' : '';
 
 //validated process exists, return throw if not exists.
