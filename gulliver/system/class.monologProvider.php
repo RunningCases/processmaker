da5b2522b3ac0bd5d4657933a24000eac47eb94b
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2018-01-15 12:00:22 +0000

    HOR-4146: Monolog improvement HOR-4303 HOR-4302 HOR-4471 HOR-4428 HOR-4146: We removed loggin_level

--- gulliver/system/class.monologProvider.php
+++ gulliver/system/class.monologProvider.php
@@ -1,52 +1,366 @@
 <?php
 
-/**
- * class.monologProvider.php
- *
-* @package gulliver.system
- *
- * ProcessMaker Open Source Edition
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- */
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+use Monolog\Processor\IntrospectionProcessor;
 
 class MonologProvider
 {
+    /**
+     * @var MonologProvider
+     */
     private static $instance = null;
-    public $aWorkspaces = null;
-    public $formatter;
-    public $streamRoutating;
-    public $registerLogger;
+    /**
+     * @var LineFormatter
+     */
+    private $formatter;
+    /**
+     * @var RotatingFileHandler
+     */
+    private $streamRoutating;
+    /**
+     * @var Logger
+     */
+    private $registerLogger;
 
     //the default format "[%datetime%] %channel%.%level_name%: %message% %context% %extra%\n";
-    public $output = "<%level%> %datetime% %channel% %level_name%: %message% %context% %extra%\n";
-    public $dateFormat = "M d H:i:s";
-    public $numOfKeepFiles = 60;
-    public $levelDebug = 100;
-    public $bubble = true;
-    public $filePermission = 0666;
+    private $output = "<%level%> %datetime% %channel% %level_name%: %message% %context% %extra%\n";
+    private $dateFormat = 'M d H:i:s';
+    /**
+     * The maximal amount of files to keep (0 means unlimited)
+     * @var int
+     */
+    private $maxFilesToKeep;
+    /**
+     * @var int level debug
+     */
+    private $levelDebug;
+    /**
+     * Whether the messages that are handled can bubble up the stack or not
+     * @var boolean
+     */
+    private $bubble = true;
+    /**
+     * @var int file permissions
+     */
+    private $filePermission;
+    /**
+     * @var string path file
+     */
+    private $pathFile;
 
-    public function __construct ($channel, $fileLog)
+    /**
+     * Logging levels from loo protocol defined in RFC 5424
+     *
+     * @var array $levels Logging levels
+     */
+    protected static $levels = [
+        'DEBUG' => 100,
+        'INFO' => 200,
+        'NOTICE' => 250,
+        'WARNING' => 300,
+        'ERROR' => 400,
+        'CRITICAL' => 500,
+        'ALERT' => 550,
+        'EMERGENCY' => 600
+    ];
+
+    public function __construct($channel, $fileLog)
     {
-        //Set Formatter
-        $this->formatter = new Monolog\Formatter\LineFormatter($this->output, $this->dateFormat);
+        // getting configuration from env.ini
+        $sysConf = System::getSystemConfiguration();
 
+        //Set level debug
+        $levelDebug = 'DEBUG';
+
+        $this->setLevelDebug($levelDebug);
+
+        //Set path where the file will be saved
+        $defaultPath = $path = PATH_DATA . 'sites' . PATH_SEP . config('system.workspace') . PATH_SEP . 'log' . PATH_SEP;
+        if (isset($sysConf['logs_location'])) {
+            $path = !empty($sysConf['logs_location']) ? $sysConf['logs_location'] : $path;
+        }
+        $this->setPathFile($path);
+
+        //Set maximal amount of files to keep (0 means unlimited)
+        $maxFilesToKeep = 60;
+        if (isset($sysConf['logs_max_files'])) {
+            $maxFilesToKeep = !empty($sysConf['logs_max_files']) ? $sysConf['logs_max_files'] : $maxFilesToKeep;
+        }
+        $this->setMaxFiles($maxFilesToKeep);
+
+        /**
+         * The permissions are normally set at the operating system level, and it's the IT administrator responsibility to set the correct file permissions
+         * It's not recommendable define in the env.ini configuration
+        */
+        $permissionFile = 0666;
+        $permissionFile = is_int($permissionFile) ? decoct($permissionFile) : $permissionFile;
+        $this->setFilePermission($permissionFile);
+
+        $this->setFormatter();
+        $this->setConfig($channel, $fileLog);
+
+        $this->testWriteLog($channel, $fileLog, [
+            $defaultPath
+        ]);
+    }
+
+    /**
+     * Test write log
+     *
+     * @param string $channel
+     * @param string $fileLog
+     * @param array $paths
+     */
+    private function testWriteLog($channel, $fileLog, $paths)
+    {
+        $fileInfo = pathinfo($fileLog);
+        $timedFilename = str_replace(
+            ['{filename}', '{date}'],
+            [$fileInfo['filename'], date('Y-m-d')],
+            '{filename}-{date}'
+        );
+
+        if (!empty($fileInfo['extension'])) {
+            $timedFilename .= '.' . $fileInfo['extension'];
+        }
+
+        if (!file_exists($this->getPathFile() . $timedFilename)) {
+            try {
+                $this->getLogger()->addInfo('Start writing the log file');
+            } catch (UnexpectedValueException $exception) {
+                //In case that the file can not be written, it will be written to the standard log file.
+                error_log($exception->getMessage());
+                if ($paths) {
+                    $path = array_shift($paths);
+                    $this->setPathFile($path);
+                    $this->setConfig($channel, $fileLog);
+                    $this->testWriteLog($channel, $fileLog, $paths);
+                }
+            } catch (Exception $exception) {
+                //In case of an exception, it will be written to the standard log file.
+                error_log($exception->getMessage());
+            }
+        }
+    }
+
+    /**
+     * Return Formatter
+     *
+     * @return LineFormatter
+     */
+    public function getFormatter()
+    {
+        return $this->formatter;
+    }
+
+    /**
+     * Set LineFormatter $formatter
+     */
+    public function setFormatter()
+    {
+        $this->formatter = new LineFormatter($this->getOutput(), $this->getDateFormat());
+    }
+
+    /**
+     * @return RotatingFileHandler
+     */
+    public function getStream()
+    {
+        return $this->streamRoutating;
+    }
+
+    /**
+     * @param string File name
+     */
+    public function setStream($fileLog)
+    {
         //Set Routating Handler
-        $this->streamRoutating = new Monolog\Handler\RotatingFileHandler($fileLog, $this->numOfKeepFiles, $this->levelDebug, $this->bubble, $this->filePermission);
-        $this->streamRoutating->setFormatter($this->formatter);
+        $this->streamRoutating = new RotatingFileHandler($this->getPathFile() . $fileLog,
+            $this->getMaxFiles(),
+            $this->getLevelDebug(),
+            $this->isBubble(),
+            $this->getFilePermissionOctDec());
 
+        $this->streamRoutating->setFormatter($this->getFormatter());
+    }
+
+    /**
+     * @return Logger
+     */
+    public function getLogger()
+    {
+        return $this->registerLogger;
+    }
+
+    /**
+     * @param string $channel The logging channel
+     */
+    public function setLogger($channel)
+    {
         //Create the channel and register the Logger with StreamRoutating
-        $this->registerLogger = new Monolog\Logger($channel);
-        $this->registerLogger->pushProcessor(new Monolog\Processor\IntrospectionProcessor());
-        $this->registerLogger->pushHandler($this->streamRoutating);
+        $this->registerLogger = new Logger($channel);
+        $this->registerLogger->pushProcessor(new IntrospectionProcessor());
+        $this->registerLogger->pushHandler($this->getStream());
+    }
 
+    /**
+     * Return format output
+     * @return string
+     */
+    public function getOutput()
+    {
+        return $this->output;
+    }
+
+    /**
+     * Set format output
+     *
+     * @param string $output
+     */
+    public function setOutput($output)
+    {
+        $this->output = $output;
+    }
+
+    /**
+     * Return date format
+     * @return string
+     */
+    public function getDateFormat()
+    {
+        return $this->dateFormat;
+    }
+
+    /**
+     * Set date format
+     * @param string $dateFormat
+     */
+    public function setDateFormat($dateFormat)
+    {
+        $this->dateFormat = $dateFormat;
+    }
+
+    /**
+     * Return is can bubble up the stack or not.
+     *
+     * @return boolean
+     */
+    public function isBubble()
+    {
+        return $this->bubble;
+    }
+
+    /**
+     * Set bubble
+     *
+     * @param boolean $bubble
+     */
+    public function setBubble($bubble)
+    {
+        $this->bubble = $bubble;
+    }
+
+    /**
+     * Return level debug
+     *
+     * @return int
+     */
+    public function getLevelDebug()
+    {
+        return $this->levelDebug;
+    }
+
+    /**
+     * Return max files
+     *
+     * @return int
+     */
+    public function getMaxFiles()
+    {
+        return $this->maxFilesToKeep;
+    }
+
+    /**
+     * Set max files
+     *
+     * @param int $maxFilesToKeep
+     */
+    public function setMaxFiles($maxFilesToKeep)
+    {
+        $this->maxFilesToKeep = $maxFilesToKeep;
+    }
+
+    /**
+     * Return permissions of file
+     *
+     * @return int
+     */
+    public function getFilePermission()
+    {
+        return $this->filePermission;
+    }
+
+    /**
+     * Returns the decimal equivalent of the octal number represented by the octal_string argument.
+     *
+     * @return int
+     */
+    public function getFilePermissionOctDec()
+    {
+        return octdec($this->filePermission);
+    }
+
+    /**
+     * Set file permissions
+     *
+     * @param int $filePermission
+     */
+    public function setFilePermission($filePermission)
+    {
+        $this->filePermission = $filePermission;
+    }
+
+    /**
+     * Returns the path where the file will be saved
+     *
+     * @return string
+     */
+    public function getPathFile()
+    {
+        return $this->pathFile;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $pathFile
+     */
+    public function setPathFile($pathFile)
+    {
+        $pathSep = '/';
+        if (strpos($pathFile, '\\') !== false) {
+            $pathSep = '\\';
+        }
+        if (substr($pathFile, -1, strlen($pathSep)) !== $pathSep) {
+            $pathFile .= $pathSep;
+        }
+        $this->pathFile = $pathFile;
+    }
+
+    /**
+     * Set level debug by string
+     *
+     * @param string $levelDebug
+     */
+    public function setLevelDebug($levelDebug)
+    {
+        $level = static::$levels['DEBUG'];
+        if (isset(static::$levels[$levelDebug])) {
+            $level = static::$levels[$levelDebug];
+        }
+        $this->levelDebug = $level;
     }
 
     /**
@@ -55,9 +369,9 @@ class MonologProvider
      * @access public
      * @return object
      */
-    public static function getSingleton ($channel, $fileLog)
+    public static function getSingleton($channel, $fileLog)
     {
-        if (self::$instance == null) {
+        if (self::$instance === null) {
             self::$instance = new MonologProvider($channel, $fileLog);
         } else {
             self::$instance->setConfig($channel, $fileLog);
@@ -69,55 +383,27 @@ class MonologProvider
      * Set channel and fileLog
      *
      * @access public
-     * @return object
+     *
+     * @param string $channel The logging channel
+     * @param string $fileLog name file
      */
-    public function setConfig ($channel, $fileLog)
+    public function setConfig($channel, $fileLog)
     {
-        //Set Routating Handler
-        $this->streamRoutating = new Monolog\Handler\RotatingFileHandler($fileLog, $this->numOfKeepFiles, $this->levelDebug, $this->bubble, $this->filePermission);
-        $this->streamRoutating->setFormatter($this->formatter);
-
-        //Create the channel and register the Logger with StreamRoutating
-        $this->registerLogger = new Monolog\Logger($channel);
-        $this->registerLogger->pushProcessor(new Monolog\Processor\IntrospectionProcessor());
-        $this->registerLogger->pushHandler($this->streamRoutating);
+        $this->setStream($fileLog);
+        $this->setLogger($channel);
     }
 
     /**
-     * to register log
+     * Register log
      *
      * @access public
-     * @return void
+     *
+     * @param int $level The logging level
+     * @param string $message The log message
+     * @param array $context The log context
      */
-    public function addLog ($level, $message, $context)
+    public function addLog($level, $message, $context)
     {
-        switch ($level) {
-            case 100://DEBUG
-                $this->registerLogger->addDebug($message, $context);
-                break;
-            case 200://INFO
-                $this->registerLogger->addInfo($message, $context);
-                break;
-            case 250://NOTICE
-                $this->registerLogger->addNotice($message, $context);
-                break;
-            case 300://WARNING
-                $this->registerLogger->addWarning($message, $context);
-                break;
-            case 400://ERROR
-                $this->registerLogger->addError($message, $context);
-                break;
-            case 500://CRITICAL
-                $this->registerLogger->addCritical($message, $context);
-                break;
-            case 550://ALERT
-                $this->registerLogger->addAlert($message, $context);
-                break;
-            case 600://EMERGENCY
-                $this->registerLogger->addEmergency($message, $context);
-                break;
-            default:
-                $this->registerLogger->addDebug($message, $context);
-        }
+        $this->getLogger()->addRecord($level, $message, $context);
     }
 }
