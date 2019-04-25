3a8d2a353b7a2f2925614ef4292a910b58907e30
dante <dante@danteloayza.localdomain>
2019-04-25 19:00:41 -0400

    Classic Processes Factory

--- database/factories/ProcessFactory.php
+++ database/factories/ProcessFactory.php
@@ -9,9 +9,41 @@ $factory->define(\ProcessMaker\Model\Process::class, function(Faker $faker) {
     /**
      * @todo Determine if we need more base columns populated
      */
-    return [
+    $process =  [
         'PRO_UID' => G::generateUniqueID(),
         'PRO_TITLE' => $faker->sentence(3),
-        'PRO_DESCRIPTION' => $faker->paragraph(3)
+        'PRO_DESCRIPTION' => $faker->paragraph(3),
+        'PRO_CREATE_USER' => '00000000000000000000000000000001',
+        'PRO_DYNAFORMS' => '',
+        'PRO_ITEE' => 1,
     ];
+
+    $task1 = factory(\ProcessMaker\Model\Task::class)
+        ->create([
+            'PRO_UID' => $process['PRO_UID'],
+            'TAS_START'=>'TRUE',
+            'TAS_TITLE'=>'Task A'
+        ]);
+
+    $task2 = factory(\ProcessMaker\Model\Task::class)
+        ->create([
+            'PRO_UID' => $process['PRO_UID'],
+            'TAS_TITLE' => 'Task B'
+        ]);
+
+    factory(\ProcessMaker\Model\Route::class)
+        ->create([
+            'PRO_UID' => $process['PRO_UID'],
+            'TAS_UID' => $task2['TAS_UID'],
+            'ROU_NEXT_TASK' => '-1',
+        ]);
+
+    factory(\ProcessMaker\Model\Route::class)
+        ->create([
+            'PRO_UID' => $process['PRO_UID'],
+            'TAS_UID' => $task1['TAS_UID'],
+            'ROU_NEXT_TASK' => $task2['TAS_UID']
+        ]);
+
+    return $process;
 });
