45e58886f1f11cbfde346ca1c3eb177e15a1772a
Paula Quispe <paula.quispe@processmaker.com>
2019-10-16 09:17:24 -0400

    PMC-1311

--- tests/unit/workflow/engine/src/ProcessMaker/BusinessModel/SkinsTest.php
+++ tests/unit/workflow/engine/src/ProcessMaker/BusinessModel/SkinsTest.php
@@ -62,17 +62,13 @@ class SkinsTest extends TestCase
      */
     public function testGetSkinsCurrentWorkspace()
     {
-        $this->object->createSkin('test2', 'test2', 'Second skin', 'ProcessMaker Team', 'current', 'neoclassic');
+        $this->object->createSkin('test2', 'test2', 'description skin', config("system.workspace"));
         $skins = $this->object->getSkins();
-
         $this->assertCount(3, $skins);
-        $this->assertEquals($skins[2]['SKIN_FOLDER_ID'], 'test2');
-        $this->assertEquals($skins[2]['SKIN_WORKSPACE'], config("system.workspace"));
-
-        $this->object->createSkin('test', 'test');
+        $this->assertEquals('test2', $skins[2]['SKIN_FOLDER_ID']);
+        $this->object->createSkin('test', 'test', 'description skin', config("system.workspace"));
         $skins = $this->object->getSkins();
-
         $this->assertCount(4, $skins);
-        $this->assertEquals($skins[3]['SKIN_FOLDER_ID'], 'test');
+        $this->assertEquals('test2', $skins[3]['SKIN_FOLDER_ID']);
     }
 }
