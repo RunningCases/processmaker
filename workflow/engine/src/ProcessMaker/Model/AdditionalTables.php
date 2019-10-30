eae2618bb47169c88596f7d48ed4dd1f16676d1e
Roly Rudy Gutierrez Pinto <gproly@gmail.com>
2019-10-30 16:28:21 -0400

    PMC-1308 New attributes in the PMTables editions

---
+++ workflow/engine/src/ProcessMaker/Model/AdditionalTables.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ProcessMaker\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AdditionalTables extends Model
+{
+    protected $table = 'ADDITIONAL_TABLES';
+    public $timestamps = false;
+
+}
