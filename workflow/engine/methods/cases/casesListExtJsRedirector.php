35d9db2c13fea6513b6e35f6dbb6f925179efcdd
Julio Cesar Laura Avendaño <juliocesar@processmaker.com>
2020-01-21 14:46:42 -0400

    PMCORE-1095

--- workflow/engine/methods/cases/casesListExtJsRedirector.php
+++ workflow/engine/methods/cases/casesListExtJsRedirector.php
@@ -42,6 +42,7 @@ if (isset($_GET['ux'])) {
     }
     /*----------------------------------********---------------------------------*/
 }
+echo "try {parent.parent.updateCasesTree();} catch(e) {}";
 ?>
 }
 </script>
