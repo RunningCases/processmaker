1bc6dfdb6acbab94412292473908e78b3bd45c28
Fabio Guachalla <fabio.guachalla@processmaker.com>
2020-02-28 12:05:04 -0400

    PMCORE-1205:Columns from process list are not correctly displayed - depending on the other columns width could get hidden
    Correction

--- workflow/engine/templates/processes/main.js
+++ workflow/engine/templates/processes/main.js
@@ -411,7 +411,7 @@ Ext.onReady(function(){
     title : '',
     stateful : true,
     stateId : 'gridProcessMain',
-    enableColumnResize: true,
+    enableColumnResize: false,
     enableHdMenu: true,
     frame:false,
     plugins: expander,
@@ -440,8 +440,8 @@ Ext.onReady(function(){
 
         {id:'PRO_UID', dataIndex: 'PRO_UID', hidden:true, hideable:false},
         {header: "", dataIndex: 'PRO_STATUS', width: 50, hidden:true, hideable:false},
-        {header: _('ID_PRO_DESCRIPTION'), dataIndex: 'PRO_DESCRIPTION',hidden:true, hideable:false},
-        {header: _('ID_PRO_TITLE'), dataIndex: 'PRO_TITLE', width: 300, renderer:function(v,p,r){
+        {header: _('ID_PRO_DESCRIPTION'), dataIndex: 'PRO_DESCRIPTION', hidden:true, hideable:false},
+        {header: _('ID_PRO_TITLE'), dataIndex: 'PRO_TITLE', width: 380, hideable:false, renderer:function(v,p,r){
             // TODO Labels for var 'type' are hardcoded, they must be replaced on the future
             var color = r.get('PROJECT_TYPE') == 'bpmn'? 'green': 'blue';
             var type = r.get('PROJECT_TYPE') == 'bpmn'? ' (BPMN Project)': '';
@@ -449,22 +449,22 @@ Ext.onReady(function(){
         }},
         {header: _('ID_TYPE'), dataIndex: 'PROJECT_TYPE', width: 60, hidden:false},
         {header: _('ID_CATEGORY'), dataIndex: 'PRO_CATEGORY_LABEL', width: 100, hidden:false},
-        {header: _('ID_STATUS'), dataIndex: 'PRO_STATUS_LABEL', width: 50, renderer:function(v,p,r){
+        {header: _('ID_STATUS'), dataIndex: 'PRO_STATUS_LABEL', width: 60, renderer:function(v,p,r){
           color = r.get('PRO_STATUS') == 'ACTIVE'? 'green': 'red';
           return String.format("<font color='{0}'>{1}</font>", color, v);
         }},
         {header: _('ID_OWNER'), dataIndex: 'PRO_CREATE_USER_LABEL', width: 150},
-        {header: _('ID_PRO_CREATE_DATE'), dataIndex: 'PRO_CREATE_DATE', width: 90},
-        {header: _('ID_INBOX'), dataIndex: 'CASES_COUNT_TO_DO', width: 50, align:'right'},
-        {header: _('ID_DRAFT'), dataIndex: 'CASES_COUNT_DRAFT', width: 50, align:'right'},
-        {header: _('ID_COMPLETED'), dataIndex: 'CASES_COUNT_COMPLETED', width: 70, align:'right'},
-        {header: _('ID_CANCELLED'), dataIndex: 'CASES_COUNT_CANCELLED', width: 70, align:'right'},
-        {header: _('ID_TOTAL_CASES'), dataIndex: 'CASES_COUNT', width: 75,renderer:function(v){return "<b>"+v+"</b>";}, align:'right'},
+        {header: _('ID_PRO_CREATE_DATE'), dataIndex: 'PRO_CREATE_DATE', width: 70},
+        {header: _('ID_INBOX'), dataIndex: 'CASES_COUNT_TO_DO', width: 60, align:'right'},
+        {header: _('ID_DRAFT'), dataIndex: 'CASES_COUNT_DRAFT', width: 60, align:'right'},
+        {header: _('ID_COMPLETED'), dataIndex: 'CASES_COUNT_COMPLETED', width: 60, align:'right'},
+        {header: _('ID_CANCELLED'), dataIndex: 'CASES_COUNT_CANCELLED', width: 60, align:'right'},
+        {header: _('ID_TOTAL_CASES'), dataIndex: 'CASES_COUNT', width: 60, renderer:function(v){return "<b>"+v+"</b>";}, align:'right'},
         {header: _('ID_PRO_DEBUG'), dataIndex: 'PRO_DEBUG_LABEL', width: 50, align:'center'}
         /*----------------------------------********---------------------------------*/
-        ,{header: _("ID_TYPE_PROCESS"), dataIndex: "PRO_TYPE_PROCESS", width: 75, align:"left"}
+        ,{header: _("ID_TYPE_PROCESS"), dataIndex: "PRO_TYPE_PROCESS", width: 70, align:"left"}
         /*----------------------------------********---------------------------------*/
-        ,{header: _("ID_LAN_UPDATE_DATE"), dataIndex: "PRO_UPDATE_DATE", width: 75, align:"left"}
+        ,{header: _("ID_LAN_UPDATE_DATE"), dataIndex: "PRO_UPDATE_DATE", width: 70, align:"left"}
       ]
     }),
     store: store,
