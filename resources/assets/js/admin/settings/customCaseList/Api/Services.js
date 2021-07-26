8a92b9f91bb50d4a6e0b0a2d1903d530ec947a5f
Rodrigo Quelca <rodrigo@processmaker.com>
2021-07-26 19:52:42 +0000

    PMCORE-3106: UI - New Custom Cases List Settings in Admin Tab
    fix CR notes

---
+++ resources/assets/js/admin/settings/customCaseList/Api/Services.js
@@ -0,0 +1,6 @@
+export default {
+    CASE_LIST_TODO: "/caseList/inbox",
+    CASE_LIST_DRAFT: "/caseList/draft",
+    CASE_LIST_UNASSIGNED: "/caseList/unassigned",
+    CASE_LIST_PAUSED: "/caseList/paused"
+};
---
+++ resources/assets/js/admin/settings/customCaseList/CustomCaseList.vue
@@ -0,0 +1,67 @@
+<template>
+    <div
+        id="home"
+    >
+        <div class="demo">
+            <div class="container">
+                <h5 >{{ $t("ID_CUSTOM_CASES_LISTS") }}</h5>
+                <div class="x_content">
+                    <b-container fluid>
+                        <b-tabs content-class="mt-3">
+                            <b-tab :title="$t('TO_DO')" active>
+                                <Tables module="inbox"/>
+                            </b-tab>
+                            <b-tab :title="$t('ID_DRAFT')" lazy>
+                                <Tables module="draft"/>
+                            </b-tab>
+                            <b-tab :title="$t('ID_UNASSIGNED')" lazy>
+                                <Tables module="unassigned"/>
+                            </b-tab>
+                             <b-tab :title="$t('ID_PAUSED')" lazy>
+                                <Tables module="paused"/>
+                            </b-tab>
+                        </b-tabs> 
+                    </b-container>
+                  
+                </div> 
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import Tables from "./Tables";
+export default {
+    name: "CustomCaseList",
+    components: {
+        Tables
+    },
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+       
+    },
+    methods: {
+        
+    }
+};
+</script>
+
+<style lang="scss">
+#home {
+    padding-left: 0px;
+    transition: 0.3s;
+}
+#home.collapsed {
+    padding-left: 50px;
+}
+#home.onmobile {
+    padding-left: 50px;
+}
+
+.container {
+    max-width: 1500px;
+}
+</style>
