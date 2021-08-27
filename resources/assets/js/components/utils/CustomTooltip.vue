0c83a764ee03f58c9bb9996b913187b72220d6a3
fabio <fabio.guachalla@processmaker.com>
2021-08-26 20:29:51 -0400

    PMCORE-3240:UI highlight marker in the menu (inbox, draft, paused and unnassigned)

--- resources/assets/js/components/utils/CustomTooltip.vue
+++ resources/assets/js/components/utils/CustomTooltip.vue
@@ -2,10 +2,16 @@
     <span
         :id="`label-${data.id}`"
         @mouseover="hoverHandler"
-        :title="labelTooltip"
+        v-b-tooltip.hover
+        @mouseleave="unhoverHandler"
+        v-bind:class="{highlightText: isHighlight}"
     >
         {{ data.title }}
-        <b-tooltip :target="`label-${data.id}`" :ref="`tooltip-${data.id}`">
+        <b-tooltip
+            :target="`label-${data.id}`"
+            triggers="hoverHandler"
+            :ref="`tooltip-${data.id}`"
+        >
             {{ labelTooltip }}
         </b-tooltip>
     </span>
@@ -17,21 +23,20 @@ import api from "./../../api/index";
 export default {
     name: "CustomTooltip",
     props: {
-        data: Object,
+        data: Object
     },
     data() {
         return {
             labelTooltip: "",
+            hovering: "",
+            show: false,
             menuMap: {
                 CASES_INBOX: "inbox",
                 CASES_DRAFT: "draft",
                 CASES_PAUSED: "paused",
-                CASES_SELFSERVICE: "unassigned",
-                todo: "inbox",
-                draft: "draft",
-                paused: "paused",
-                unassigned: "unassigned",
+                CASES_SELFSERVICE: "unassigned"
             },
+            isHighlight: false
         };
     },
     methods: {
@@ -39,7 +44,7 @@ export default {
          * Delay the hover event
          */
         hoverHandler() {
-            this.setTooltip();
+            this.hovering = setTimeout(() => { this.setTooltip() }, 3000);
         },
         /**
          * Reset the delay and hide the tooltip
@@ -48,6 +53,7 @@ export default {
             let key = `tooltip-${this.data.id}`;
             this.labelTooltip = "";
             this.$refs[key].$emit("close");
+            clearTimeout(this.hovering);
         },
         /**
          * Set the label to show in the tooltip
@@ -58,8 +64,20 @@ export default {
                 let key = `tooltip-${that.data.id}`;
                 that.labelTooltip = response.data.label;
                 that.$refs[key].$emit("open");
+                that.isHighlight = false;
             });
         },
+        /**
+         * Set bold the label 
+         */
+        setHighlight() {
+            this.isHighlight = true;
+        }
     },
 };
 </script>
+<style>
+.highlightText {
+    font-weight: 900;
+}
+</style>
